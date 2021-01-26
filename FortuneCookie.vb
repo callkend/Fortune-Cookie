e00d1ba0b03b5e4238d64fe58a20b721261fc4ef
callkend <77285430+callkend@users.noreply.github.com>
2021-01-26 08:33:12 -0700

    Add project files.

---
+++ FortuneCookie.vb
@@ -0,0 +1,24 @@
+﻿Module FortuneCookie
+
+    Sub main()
+        Dim randomNumber As Int16
+
+        'Generates a random number.
+        VBMath.Randomize()
+        randomNumber = Int(3 * Rnd() + 1)
+
+        'random number generates a fortune
+        Select Case randomNumber
+            Case 1
+                Console.WriteLine("A friend asks only for your time not your money.")
+            Case 2
+                Console.WriteLine("A faithful friend is a strong defense.")
+            Case 3
+                Console.WriteLine("A dubious friend may be an enemy in camouflage.")
+
+        End Select
+
+        Console.ReadLine()
+    End Sub
+
+End Module
